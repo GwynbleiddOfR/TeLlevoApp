f5f91512f042acb574c91ef66d7e705b357bcbb3
Francisco Castillo <fr.castillou@duocuc.cl>
2024-09-23 17:54:34 -0300

    Agregado ajustes

---
+++ src/app/pages/ajustes/ajustes.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AjustesPage } from './ajustes.page';
+
+describe('AjustesPage', () => {
+  let component: AjustesPage;
+  let fixture: ComponentFixture<AjustesPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AjustesPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/ajustes/ajustes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajustes',
+  templateUrl: './ajustes.page.html',
+  styleUrls: ['./ajustes.page.scss'],
+})
+export class AjustesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
