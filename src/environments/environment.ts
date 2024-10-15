55ca10d5d616eddb54d0e9aa4bac2ffd9cfd9b62
CETECOM <cetecom_conce@duoc.cl>
2024-10-15 16:39:28 -0300

    Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,24 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ4DyHR3OxRm9g7M2AZF7PIpQYpsSSfsM",
+  authDomain: "tellevoapp-efcd2.firebaseapp.com",
+  projectId: "tellevoapp-efcd2",
+  storageBucket: "tellevoapp-efcd2.appspot.com",
+  messagingSenderId: "874102776496",
+  appId: "1:874102776496:web:0a52c85bd8730e5d131175",
+  measurementId: "G-PLYL9XJN3P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
