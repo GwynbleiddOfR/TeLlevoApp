f9a4f9901b3fc06744b9430de847dd7e81da6f97
CETECOM <cetecom_conce@duoc.cl>
2024-10-29 16:47:59 -0300

    Botones de zoom agregados

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -10,7 +10,7 @@ import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 export class MapPage implements OnInit {
   map: mapboxgl.Map;
   userMarker: mapboxgl.Marker;
-  currentLocation: [number, number];
+  currentLocation: [number, number] = [-74.5, 40];
 
   constructor(private geolocation: Geolocation) {}
 
@@ -21,31 +21,53 @@ export class MapPage implements OnInit {
 
   initializeMap() {
     (mapboxgl as any).accessToken = 'pk.eyJ1Ijoic2VhcGlsZW8iLCJhIjoiY20ybnJpZTlhMDlqNzJscHU2NjF1enptMCJ9.aWtLWdpfsRCMSZwJeu_anQ';
+    const savedLocation = localStorage.getItem('mapLocation');
+    const savedZoom = localStorage.getItem('mapZoom');
+
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40], // Coordenadas iniciales
-      zoom: 14,
+      center: savedLocation ? JSON.parse(savedLocation) : this.currentLocation,
+      zoom: savedZoom ? Number(savedZoom) : 14,
     });
+
+    this.map.on('moveend', () => {
+      const center = this.map.getCenter();
+      localStorage.setItem('mapLocation', JSON.stringify([center.lng, center.lat]));
+    });
+
+    this.map.on('zoomend', () => {
+      const zoom = this.map.getZoom();
+      localStorage.setItem('mapZoom', zoom.toString());
+    });
+  }
+
+  zoomIn() {
+    const currentZoom = this.map.getZoom();
+    this.map.setZoom(currentZoom + 1);
+  }
+
+  zoomOut() {
+    const currentZoom = this.map.getZoom();
+    this.map.setZoom(currentZoom - 1);
   }
 
   trackUserLocation() {
     this.geolocation.watchPosition().subscribe((position) => {
-      // Verifica si el objeto es de tipo Geoposition y contiene coords
       if ('coords' in position) {
         const lng = position.coords.longitude;
         const lat = position.coords.latitude;
         this.currentLocation = [lng, lat];
 
         if (this.userMarker) {
-          this.userMarker.setLngLat(this.currentLocation); // Actualiza la ubicación del marcador
+          this.userMarker.setLngLat(this.currentLocation);
         } else {
           this.userMarker = new mapboxgl.Marker()
             .setLngLat(this.currentLocation)
-            .addTo(this.map); // Crea un nuevo marcador si no existe
+            .addTo(this.map);
         }
 
-        this.map.setCenter(this.currentLocation); // Centra el mapa en la ubicación actual
+        this.map.setCenter(this.currentLocation);
       } else {
         console.error('Error obteniendo la ubicación:', position);
       }
