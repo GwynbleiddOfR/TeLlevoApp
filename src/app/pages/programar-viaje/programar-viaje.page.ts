a4613727e3a2c652e538ec96d1e1b79aeeace221
CETECOM <cetecom_conce@duoc.cl>
2024-11-12 16:35:22 -0300

    Mapa

--- src/app/pages/programar-viaje/programar-viaje.page.ts
+++ src/app/pages/programar-viaje/programar-viaje.page.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
+import mapboxgl from 'mapbox-gl';
+import * as mapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 @Component({
   selector: 'app-programar-viaje',
   templateUrl: './programar-viaje.page.html',
@@ -10,14 +12,14 @@ import { UtilsService } from 'src/app/services/utils.service';
 })
 export class ProgramarViajePage implements OnInit {
   formularioViaje!: FormGroup;
-
+  map: mapboxgl.Map;
   constructor(private fb: FormBuilder, private navCtrl: NavController) { }
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
   ngOnInit() {
     this.formularioViaje = this.fb.group({
       horaSalida: [new Date().toISOString(), Validators.required],
-      destino: ['', [Validators.required, Validators.minLength(3)]],
+      destino: ['', [Validators.required]],
       costo: ['', [Validators.required, Validators.min(1)]],
     });
   }
@@ -41,4 +43,34 @@ export class ProgramarViajePage implements OnInit {
       }
     }
   }
+
+  initializeMap() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2VhcGlsZW8iLCJhIjoiY20ybnJpZTlhMDlqNzJscHU2NjF1enptMCJ9.aWtLWdpfsRCMSZwJeu_anQ'; // Sustituye con tu token de Mapbox
+
+    this.map = new mapboxgl.Map({
+      container: 'map', // El ID del contenedor del mapa en el HTML
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40], // Coordenadas iniciales
+      zoom: 9
+    });
+    const geocoder = new mapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      marker: {
+        color: 'orange'
+      },
+      mapboxgl: mapboxgl
+    });
+
+    this.map.addControl(geocoder);
+    geocoder.on('result', (e) => {
+      const lngLat = e.result.geometry.coordinates;
+      this.map.flyTo({
+        center: lngLat,
+        essential: true, // Hace que el movimiento sea fluido
+        zoom: 14
+      });
+      this.formularioViaje.controls['destino'].setValue(e.result.place_name);
+    })
+  }
+
 }
