071c169490fb965aedaaa7eb7cc92cb76eeb26e7
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-11-13 20:11:49 -0300

    Nombres en el chat OK

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChatService } from 'src/app/services/chat.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,12 +13,16 @@ export class ChatPage implements OnInit {
   viajeId: string;
   messages: any[] = [];
   newMessage: string = '';
+  userDisplayName: string | null = null;
 
-  constructor(private route: ActivatedRoute, private chatService: ChatService) {}
+  constructor(private route: ActivatedRoute, private chatService: ChatService, private firebaseService: FirebaseService) { }
 
   ngOnInit() {
     this.viajeId = this.route.snapshot.paramMap.get('id')!;
     this.subscribeToChat(this.viajeId);
+    this.firebaseService.getCurrentUserDisplayName().then(displayName => {
+      this.userDisplayName = displayName;
+    });
   }
 
   subscribeToChat(viajeId: string) {
@@ -28,7 +33,7 @@ export class ChatPage implements OnInit {
 
   sendMessage() {
     if (this.newMessage) {
-      const user = 'Usuario'; // Aquí puedes obtener el nombre del usuario
+      const user = this.userDisplayName || 'Usuario'; // Utiliza el nombre del usuario o 'Usuario' por defecto
       this.chatService.sendMessage(this.viajeId, user, this.newMessage);
       this.newMessage = ''; // Limpiar el input
     }
