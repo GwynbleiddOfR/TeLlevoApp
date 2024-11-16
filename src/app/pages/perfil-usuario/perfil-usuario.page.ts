29848217c183435e2d12299d84a9056f192b01b3
Francisco Castillo <fr.castillou@duocuc.cl>
2024-11-16 01:57:37 -0300

    Fixes

--- src/app/pages/perfil-usuario/perfil-usuario.page.ts
+++ src/app/pages/perfil-usuario/perfil-usuario.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -10,26 +11,39 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 export class PerfilUsuarioPage implements OnInit {
 
   userData: any;
+  vehiculoData: any;
+
   constructor(private router: Router) { }
-  firebaseSvc = inject(FirebaseService)
-  async ngOnInit() {
+
+  firebaseSvc = inject(FirebaseService);
+  utils = inject(UtilsService);
+
+  ngOnInit() {
+    this.obtenerDatosUsuario();
+  }
+
+  async obtenerDatosUsuario() {
+    const loading = await this.utils.loading(); // Mostrar loading
+    loading.present();
     try {
       const user = await this.firebaseSvc.auth.currentUser;
-
       if (user) {
         const path = `users/${user.uid}`;
-
+        console.log(user.uid);
         this.userData = await this.firebaseSvc.getDocument(path);
-        console.log("User data:", this.userData);
-
+        this.firebaseSvc.obtenerVehiculoPorUserId(user.uid).subscribe((vehiculos) => {
+          this.vehiculoData = vehiculos[0];
+        });
+        console.log(this.vehiculoData);
       } else {
         console.warn("No user is currently logged in.");
       }
     } catch (error) {
       console.error("Error fetching user data:", error);
+    } finally {
+      loading.dismiss(); // Ocultar loading al finalizar
     }
   }
-
   verHistorial() {
     console.log('Accediendo al historial de viajes...');
     this.router.navigate(['/historial-viajes']);
